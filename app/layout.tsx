bc77127c7f0b929e020e7ab8d4de48b013f8b2bb
MaximRahimkhanov <zloyigrog@gmail.com>
2026-03-28 15:06:43 +0200

    add styles for backGround

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import TanStackProvider from './components/TanStackProvider/TanStackProvider';
 import Header from './components/Header/Header';
 
-import './styles/globals.scss';
 import './styles/reset.scss';
+import './styles/globals.scss';
 
 export const metadata: Metadata = {
   title: 'AI ManagerDB',
